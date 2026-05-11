d88c8fdd06dc0e89d668562f42dd2300de5900d9
ProbablyRaging <probablyraging@gmail.com>
2026-05-11 21:11:05 +1000

    improve widgets

--- src-tauri/gen/android/app/src/main/java/com/zevnda/woland/WolBroadcastReceiver.kt
+++ src-tauri/gen/android/app/src/main/java/com/zevnda/woland/WolBroadcastReceiver.kt
@@ -8,25 +8,25 @@ import java.net.DatagramSocket
 import java.net.InetAddress
 
 class WolBroadcastReceiver : BroadcastReceiver() {
-  override fun onReceive(
-    context: Context,
-    intent: Intent,
-  ) {
-    val mac = intent.getStringExtra("device_mac") ?: return
-    val ip = intent.getStringExtra("device_ip") ?: return
-    val port = intent.getStringExtra("device_port")?.toIntOrNull() ?: 9
+    override fun onReceive(
+        context: Context,
+        intent: Intent,
+    ) {
+        val mac = intent.getStringExtra("device_mac") ?: return
+        val ip = intent.getStringExtra("device_ip") ?: return
+        val port = intent.getStringExtra("device_port")?.toIntOrNull() ?: 9
 
-    Thread {
-      try {
-        val macBytes = mac.split(":", "-").map { it.toInt(16).toByte() }.toByteArray()
-        val packet = ByteArray(6) { 0xFF.toByte() } + (0 until 16).flatMap { macBytes.toList() }.toByteArray()
-        val socket = DatagramSocket()
-        socket.broadcast = true
-        socket.send(DatagramPacket(packet, packet.size, InetAddress.getByName(ip), port))
-        socket.close()
-      } catch (e: Exception) {
-        e.printStackTrace()
-      }
-    }.start()
-  }
+        Thread {
+            try {
+                val macBytes = mac.split(":", "-").map { it.toInt(16).toByte() }.toByteArray()
+                val packet = ByteArray(6) { 0xFF.toByte() } + (0 until 16).flatMap { macBytes.toList() }.toByteArray()
+                val socket = DatagramSocket()
+                socket.broadcast = true
+                socket.send(DatagramPacket(packet, packet.size, InetAddress.getByName(ip), port))
+                socket.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }.start()
+    }
 }
